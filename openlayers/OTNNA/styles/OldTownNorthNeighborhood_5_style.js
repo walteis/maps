a689d8b66a781956f1ebcfb0b2f0565c7b2d68a3
walteis <walt.eis@gmail.com>
2026-02-20 19:49:47 -0600

    Add files via upload

---
+++ openlayers/OTNNA/styles/OldTownNorthNeighborhood_5_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_OldTownNorthNeighborhood_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(41,58,255,0.686)', lineDash: [60.8,30.4,15.2,30.4], lineCap: 'square', lineJoin: 'bevel', width: 15.2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
